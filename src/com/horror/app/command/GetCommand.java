b8c35cca8009e2b5af4c52ecbaf675f91ba327b5
Sarthak Oza <Sarthak.Oza@tlgcohort.com>
2023-11-29 13:28:14 -0800

    refactored get command class, AB#175

--- src/com/horror/app/command/GetCommand.java
+++ src/com/horror/app/command/GetCommand.java
@@ -22,8 +22,6 @@ class GetCommand implements Command {
                 // If furniture, print a message that it can't be picked up
                 System.out.printf("You can't pick up %s. It's part of the room.\n", noun);
             }
-
-            System.out.println(Controller.getInstance().getPlayer().getInventory());
         }
     }
 
